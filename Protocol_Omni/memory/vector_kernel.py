34878955565c747a8b965f7f3232fa91ff0d83f9
freeemc2 <freeemc2@yahoo.com>
2026-01-05 13:24:48 -0500

    Delete Protocol_Omni directory

--- Protocol_Omni/memory/vector_kernel.py
+++
@@ -1,9 +0,0 @@
-import chromadb
-class MemoryKernel:
-    def __init__(self):
-        self.client = chromadb.PersistentClient(path="./omni_memory")
-        self.collection = self.client.get_or_create_collection("sovereign_history")
-    
-    def store_goal(self, goal, step):
-        # The 30-Year Rule: Write it down + One Step
-        self.collection.add(documents=[goal], metadatas=[{"step": step}], ids=[str(hash(goal))])
